1aa19155c2ad15f9f574c7998ae1e288f968c5b7
Chumki khatun <chumkikhatun370@gmail.com>
2024-08-15 21:34:05 +0600

    add neutral color and outfit font

--- tailwind.config.js
+++ tailwind.config.js
@@ -26,17 +26,20 @@ module.exports = {
         accent: "#F8F7F1", // Replace with the actual hex code for Accent Color
         white: "#ffffff",
         neutral: {
-          1: "#f8f9fa", // Replace with actual hex code for Neutral 1
-          2: "#ebedef", // Replace with actual hex code for Neutral 2
-          3: "#dee2e6", // Replace with actual hex code for Neutral 3
-          4: "#ced4da", // Replace with actual hex code for Neutral 4
-          5: "#adb5bd", // Replace with actual hex code for Neutral 5
-          6: "#6c757d", // Replace with actual hex code for Neutral 6
-          7: "#495057", // Replace with actual hex code for Neutral 7
-          8: "#343a40", // Replace with actual hex code for Neutral 8
-          9: "#212529", // Replace with actual hex code for Neutral 9
-          10: "#0b0d0e", // Replace with actual hex code for Neutral 10
+          1: "#E6E9ED", // Replace with actual hex code for Neutral 1
+          2: "#CCD3DB", // Replace with actual hex code for Neutral 2
+          3: "#B3BEC9", // Replace with actual hex code for Neutral 3
+          4: "#99A8B7", // Replace with actual hex code for Neutral 4
+          5: "#8092A4", // Replace with actual hex code for Neutral 5
+          6: "#677C92", // Replace with actual hex code for Neutral 6
+          7: "#4D6680", // Replace with actual hex code for Neutral 7
+          8: "#34516E", // Replace with actual hex code for Neutral 8
+          9: "#1A3B5C", // Replace with actual hex code for Neutral 9
+          10: "#01254A", // Replace with actual hex code for Neutral 10
         },
+      },
+      fontFamily: {
+        outfit: ["Outfit", "sans-serif"],
       },
     },
   },
